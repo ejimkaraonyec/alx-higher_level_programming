04b03b5432763d8b906777c56a8eda7aebadec6d
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-12-05 12:36:39 -0800

    update script

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list=[]):
-    """Find all multiples of two in a list"""
-    if my_list == '':
-        return(None)
 
-    new_list = my_list.copy()
-    for idx, val in enumerate(new_list):
-        if val % 2 is 0:
-            new_list[idx] = True
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list."""
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
         else:
-            new_list[idx] = False
-    return(new_list)
+            multiples.append(False)
+
+    return (multiples)
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    copy_string = my_string[:]
-    ch_list = list(copy_string)
-    for i in range(len(ch_list)):
-        if i < len(ch_list):
-            if ch_list[i] == "c" or ch_list[i] == "C":
-                del ch_list[i]
-    new_string = ''.join(ch_list)
-    return(new_string)
+    counts = my_string.count('c')
+    new_string = list(my_string)
+    while counts:
+        new_string.remove('c')
+        counts -= 1
+    counts = my_string.count('C')
+    while counts:
+        new_string.remove('C')
+        counts -= 1
+    new_string = ''.join(new_string)
+    return new_string

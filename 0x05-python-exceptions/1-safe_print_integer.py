0ecbc6bdb89780e0233526b854e9104209b50dde
ejimkaraonyec <ejimkaraonyec@gmail.com>
2022-12-20 02:12:46 -0800

    upload scripts

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return(False)
-    else:
-        return(True)
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,20 +2,19 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
-
-    for i in range(list_length):
+    new_list = []
+    for i in range(0, list_length):
         try:
-            number = (my_list_1[i] / my_list_2[i])
-        except ZeroDivisionError:
-            print("division by 0")
-            number = 0
+            div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            number = 0
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
         except IndexError:
             print("out of range")
-            number = 0
+            div = 0
         finally:
-            result.append(number)
-    return(result
+            new_list.append(div)
+    return (new_list)

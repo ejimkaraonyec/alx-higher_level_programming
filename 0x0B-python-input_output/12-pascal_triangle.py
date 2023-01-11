b3133f97a2eeaa8b72bed82c3d5ae5472f4a1aa9
ejimkaraonyec <ejimkaraonyec@gmail.com>
2023-01-11 01:47:29 -0800

    update pascal t's scrtipt

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """Defines a Pascal's Triangle function."""
 
-
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-    Returns a list of lists of integers representing the triangle.
-    """
     if n <= 0:
-        return []
+        return([])
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return
+    plist = []
+    if n > 1:
+        for r in range(n):
+            plist.append(get_row(r))
+
+    return(plist)
+
+
+def get_row(n):
+    rowstr = (str(11 ** n))
+    return([num for num in rowstr])

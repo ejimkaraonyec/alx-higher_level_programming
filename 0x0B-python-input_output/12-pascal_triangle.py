7d88a34338f665b5e266e7c3cd8f4ee2017c2e55
ejimkaraonyec <ejimkaraonyec@gmail.com>
2023-01-11 01:51:18 -0800

    update pascal t's scrtipt

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """Pascal triangle
-    Args:
-        n (int): number of rows
-    Returns:
-        matrix representing Pascal's triangle
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
     """
-    matrix = []
     if n <= 0:
-        return matrix
-    else:
-        for r in range(n):
-            if r == 0:
-                matrix.append([1])
-            elif r == 1:
-                matrix.append([1, 1])
-            else:
-                matrix.append([1 for x in range(r + 1)])
-                for c in range(1, len(matrix[r]) - 1):
-                    matrix[r][c] = matrix[r - 1][c - 1] + matrix[r - 1][c]
+        return []
 
-    return matrix
-
-
-if __name__ == '__main__':
-    def print_triangle(triangle):
-        """
-        Print the triangle
-        """
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(8))
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
